c18e948a6766825c1c0d0adab5cefa74bbae5323
XFM Operator <xf04bm@xf04bm-ws1.cs.nsls2.local>
2019-07-15 16:27:08 -0400

    XandYmotors

--- startup/11-DAC.py
+++ startup/11-DAC.py
@@ -1,9 +1,11 @@
 from ophyd import EpicsMotor, Device, Component as Cpt
 
 class DiamondAnvil(Device):
-    x = Cpt(EpicsMotor, 'C-ES{DET:1-Ax:Z}Mtr')
-    y = Cpt(EpicsMotor, 'C-ES{DET:1-Ax:Y}Mtr')
-    z = Cpt(EpicsMotor, '-ES:1:m7')
+    x = Cpt(EpicsMotor, '-ES:2:m1')
+#    x = Cpt(EpicsMotor, 'C-ES{DET:1-Ax:Z}Mtr')
+    y = Cpt(EpicsMotor, '-ES:2:m2')
+#    y = Cpt(EpicsMotor, 'C-ES{DET:1-Ax:Y}Mtr')
+    z = Cpt(EpicsMotor, '-ES:2:m3')
 
 #DACx = EpicsMotor('XF:04BMC-ES{DET:1-Ax:Z}Mtr', name='DAC X')
 #DACy = EpicsMotor('XF:04BMC-ES{DET:1-Ax:Y}Mtr', name='DAC Y')
