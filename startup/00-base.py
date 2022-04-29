e48eb01f4f131636a24715aae5861a90def4049f
XFN Operator <xf04bm@xf04bm-ws2.nsls2.bnl.local>
2022-04-29 15:58:13 -0400

    turn on kafka publishing

--- startup/00-base.py
+++ startup/00-base.py
@@ -1,5 +1,9 @@
 import nslsii
-nslsii.configure_base(get_ipython().user_ns, 'xfm')
+
+nslsii.configure_base(get_ipython().user_ns, 
+        'xfm',
+        publish_documents_with_kafka=True)
+
 #nslsii.configure_olog(get_ipython().user_ns)
 
 #Optional: set any metadata that rarely changes.
