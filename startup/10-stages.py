ab3bf728d157101aec51fcb1864d039e911403d5
Thomas A Caswell <tcaswell@bnl.gov>
2022-06-03 11:39:03 -0400

    MNT: remove maia specific devices and plans

--- startup/10-stages.py
+++ startup/10-stages.py
@@ -9,15 +9,3 @@ S = Stage('XF:04BMC-ES:2', name='S')
 S_x   = S.x
 S_y   = S.y
 S_z   = S.z
-
-class MaiaStage(Device):
-    x    = Cpt(EpicsMotor, '{PI180:1-Ax:MaiaX}Mtr')
-    y    = Cpt(EpicsMotor, '{PI180:1-Ax:MaiaY}Mtr')
-    z    = Cpt(EpicsMotor, '{PI180:1-Ax:MaiaZ}Mtr')
-    r    = Cpt(EpicsMotor, '{SR50pp:1-Ax:MaiaR}Mtr')
-
-M = MaiaStage('XF:04BMC-ES:2', name='M')
-M_x   = M.x
-M_y   = M.y
-M_z   = M.z
-M_r   = M.r
